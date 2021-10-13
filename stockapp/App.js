42c672ee40fceb7832d558e175b59a019b7abbad
hrishabh <hrishabhm22@gmail.com>
2021-10-13 23:13:07 +0530

    Navigation Added in App.js

--- stockapp/App.js
+++ stockapp/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import {Text} from 'native-base';
-import SignIn from './src/screens/SignIn';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './src/screens/Home';
+import Detail from './src/screens/Detail';
+import {StatusBar} from 'react-native';
+import Profile from './src/screens/Profile';
 import SignUp from './src/screens/SignUp';
+import SignIn from './src/screens/SignIn';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <>
-      <SignIn />
-      {/* <SignUp /> */}
+      <StatusBar backgroundColor="#3700B3" />
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="SignIn"
+            screenOptions={{
+              headerShown: false,
+            }}>
+            <Stack.Screen name="Profile" component={Profile} />
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Detail" component={Detail} />
+            <Stack.Screen name="SignUp" component={SignUp} />
+            <Stack.Screen name="SignIn" component={SignIn} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
     </>
   );
 };
