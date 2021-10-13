3ca84bdcb28b43fd49283014f34279e0e3399b0e
hrishabh <hrishabhm22@gmail.com>
2021-10-13 22:45:24 +0530

    SignIn UI Created

---
+++ stockapp/src/screens/SignIn.js
@@ -0,0 +1,130 @@
+import React, {useState} from 'react';
+import {
+  VStack,
+  Input,
+  Button,
+  Heading,
+  Box,
+  Link,
+  HStack,
+  Text,
+  Icon,
+  Center,
+  PresenceTransition,
+  ScrollView,
+} from 'native-base';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+
+const SignIn = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  const showPasswordHandler = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleLogin = async () => {
+    //TODO: Finish this func
+  };
+
+  return (
+    <>
+      <ScrollView>
+        <Center>
+          <Heading mt="4" mb="2" fontSize="50" pl="6" fontFamily="festive">
+            Welcome
+          </Heading>
+          <Heading size="xs" fontWeight="600" pl="6" color="gray.500">
+            Your One Step Solution to Stocks
+          </Heading>
+        </Center>
+        <PresenceTransition
+          visible={true}
+          initial={{
+            opacity: 0,
+            scale: 0,
+          }}
+          animate={{
+            opacity: 1,
+            scale: 1,
+            transition: {
+              duration: 500,
+            },
+          }}>
+          <VStack flex={1} space="4" mt="30%">
+            <Box
+              p="2"
+              py="8"
+              w="90%"
+              mx="auto"
+              borderWidth="1"
+              borderRadius="xl"
+              overflow="hidden"
+              borderColor="coolGray.500">
+              <Input
+                placeholder="Email"
+                m="2"
+                size="xl"
+                value={email}
+                onChangeText={text => setEmail(text)}
+                InputLeftElement={
+                  <Icon
+                    as={<MaterialIcon name="email" />}
+                    size={7}
+                    ml={2}
+                    color="muted.400"
+                  />
+                }
+              />
+              <Input
+                placeholder="Password"
+                m="2"
+                size="xl"
+                value={password}
+                onChangeText={text => setPassword(text)}
+                type={showPassword ? 'text' : 'password'}
+                InputRightElement={
+                  <Icon
+                    onPress={showPasswordHandler}
+                    as={
+                      <MaterialIcon
+                        name={showPassword ? 'visibility' : 'visibility-off'}
+                      />
+                    }
+                    size={7}
+                    mr={2}
+                    color="muted.400"
+                  />
+                }
+              />
+              <Button mt="4" size="lg" onPress={handleLogin}>
+                Login
+              </Button>
+              <Center>
+                <HStack space={2} flex={1} mt="4">
+                  <Text fontSize="sm" color="muted.700" fontWeight={400}>
+                    New User
+                  </Text>
+                  <Link
+                    _text={{
+                      color: 'indigo.500',
+                    }}
+                    _hover={{
+                      cursor: 'pointer',
+                    }}
+                    cursor="pointer"
+                    onPress={() => {}}>
+                    Signup
+                  </Link>
+                </HStack>
+              </Center>
+            </Box>
+          </VStack>
+        </PresenceTransition>
+      </ScrollView>
+    </>
+  );
+};
+
+export default SignIn;
