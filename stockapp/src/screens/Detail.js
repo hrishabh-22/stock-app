0e38931a7328be14f1dbd20b958ba117ea2aef50
hrishabh <hrishabhm22@gmail.com>
2021-10-13 23:17:56 +0530

    Navigation error fixed and raw ui made

---
+++ stockapp/src/screens/Detail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'native-base';
+
+const Detail = () => {
+  return (
+    <>
+      <Text>Detail</Text>
+    </>
+  );
+};
+
+export default Detail;
---
+++ stockapp/src/screens/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'native-base';
+
+const Home = () => {
+  return (
+    <>
+      <Text>Home</Text>
+    </>
+  );
+};
+
+export default Home;
