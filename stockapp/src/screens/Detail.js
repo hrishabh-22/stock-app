65b34d9e01391fc250c90f008d6790af06b5a1ef
hrishabh <hrishabhm22@gmail.com>
2021-10-14 09:15:32 +0530

    Api Call made in Detail Page

--- stockapp/src/screens/Detail.js
+++ stockapp/src/screens/Detail.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import {Text} from 'native-base';
+import React, {useState, useEffect} from 'react';
+import CustomHeader from '../layout/CustomHeader';
+import {fetchDetails} from '../utils/ApiCalls';
+import {
+  ScrollView,
+  HStack,
+  Text,
+  VStack,
+  Heading,
+  Center,
+  Spinner,
+} from 'native-base';
 
-const Detail = () => {
+const Detail = ({navigation, route}) => {
+  const [details, setDetails] = useState(null);
+
+  const loadData = () => {
+    const {stockid} = route.params;
+    fetchDetails(stockid)
+      .then(data => setDetails(data))
+      .catch(err => console.log(err));
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <>
-      <Text>Detail</Text>
+      <CustomHeader displayAllBtn={false} />
     </>
   );
 };
